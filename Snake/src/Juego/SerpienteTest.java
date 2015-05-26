f4bcf285d8f18036055c3462e2193e0bd5bdd603
dani <dani@dani-Lenovo-G50-30>
2015-05-26 02:05:02 +0200

    test finalizados

--- Snake/src/Juego/SerpienteTest.java
+++ Snake/src/Juego/SerpienteTest.java
@@ -2,33 +2,62 @@ package Juego;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class SerpienteTest {
 
+	Puntos p = new Puntos();
+	Tabla t = new Tabla(p);
+	Serpiente s = new Serpiente(t);
+
 	@Test
 	public void testSerpiente() {
-		fail("Not yet implemented");
+		assertEquals(s.getClass(), Serpiente.class);
 	}
 
 	@Test
 	public void testAvance() {
-		fail("Not yet implemented");
+		int col = Tabla.NUMCOLS / 2;
+		assertEquals(s.getCuerpo().get(0).getCol(), col);
+		s.avance();
+		col++;
+		assertEquals(s.getCuerpo().get(0).getCol(), col);
+		s.avance();
+		col++;
+		assertEquals(s.getCuerpo().get(0).getCol(), col);
+		s.avance();
+		col++;
+		assertEquals(s.getCuerpo().get(0).getCol(), col);
+	}
+
+	@Before
+	public void direccionDerecha() {
+		s.setDireccion(Direccion.est);
 	}
 
 	@Test
 	public void testGetDireccion() {
-		fail("Not yet implemented");
+		assertEquals(s.getDireccion(), Direccion.est);
+		s.setDireccion(Direccion.west);
+		assertEquals(s.getDireccion(), Direccion.west);
+		s.setDireccion(Direccion.north);
+		assertEquals(s.getDireccion(), Direccion.north);
 	}
 
 	@Test
 	public void testSetDireccion() {
-		fail("Not yet implemented");
+		assertEquals(s.getDireccion(), Direccion.est);
+		s.setDireccion(Direccion.north);
+		assertEquals(s.getDireccion(), Direccion.north);
+		s.setDireccion(Direccion.south);
+		assertEquals(s.getDireccion(), Direccion.south);
 	}
 
 	@Test
 	public void testGetCuerpo() {
-		fail("Not yet implemented");
+		assertEquals(s.getCuerpo().get(0).getClass(), Nodo.class);
+		assertEquals(s.getCuerpo().get(1).getClass(), Nodo.class);
+		assertEquals(s.getCuerpo().get(5).getClass(), Nodo.class);
 	}
-
 }
